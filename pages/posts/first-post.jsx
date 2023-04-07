8d61ae26e47d9010e925f9bf4c1610ec26eab9b4
Calvin Chiramal <calvin.chiramal@bigbinary.com>
2023-04-07 13:55:44 +0530

    Created basic layout.

---
+++ pages/posts/first-post.jsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Link from "next/link";
+
+import Layout from "../../components/layout";
+
+const FirstPost = () => (
+  <Layout>
+    <Head>
+        <title>First Post</title>
+        <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <h1>First Post</h1>
+  </Layout>
+)
+
+export default FirstPost;
